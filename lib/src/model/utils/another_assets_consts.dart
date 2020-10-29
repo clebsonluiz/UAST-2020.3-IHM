a7f53c97f0fc07d4795414e5c910da92b1eff896
clebsonluiz <clebsonluiz5@gmail.com>
2020-10-28 21:09:23 -0300

    Commit 38 - Removendo ccódigo não utilizado

--- lib/src/model/utils/another_assets_consts.dart
+++ lib/src/model/utils/another_assets_consts.dart
@@ -10,12 +10,9 @@ mixin AnotherConsts {
 
   static const String PORTAS = "portas.png";
 
-  static const String BG_MENU_CAVE = 'bg_menu.png';
 
-  static const String BG_OBJETIVE_1 = 'objetivo_fundo_1.png';
   static const String BG_OBJETIVE_2 = 'objetivo_fundo_2.png';
 
-  static const String BG_SIMBOLO_1 = 'fundo_simbolo_1.png';
   static const String BG_SIMBOLO_2 = 'fundo_simbolo_2.png';
 
   static const String BG_TIMER = 'fundo_timer.png';
@@ -47,10 +44,7 @@ mixin AnotherConsts {
     HEARTH_STATUS,
     KEYS,
     PORTAS,
-    BG_MENU_CAVE,
-    BG_OBJETIVE_1,
     BG_OBJETIVE_2,
-    BG_SIMBOLO_1,
     BG_SIMBOLO_2,
     MENU_ITEM_1,
     MENU_ITEM_2,
